2cf1f6be9d5670d6452fd9f086317fb875b8eade
blue linden <dev@bluelinden.art>
2023-04-24 16:10:45 -0400

    create footer renderer on main page

--- src/frontend/main-page.ts
+++ src/frontend/main-page.ts
@@ -3,6 +3,8 @@ import Handlebars from "handlebars";
 // @ts-ignore
 import Header from "./layout/header.dry.hbs";
 // @ts-ignore
+import Footer from "./layout/footer.dry.hbs";
+// @ts-ignore
 import HeaderContain from "./layout/headercontainer.dry.hbs";
 // @ts-ignore
 import Main from "./layout/main.dry.hbs";
@@ -15,15 +17,39 @@ import SubTitle from "./components/subtitle.dry.hbs";
 // @ts-ignore
 import ColumnSelect from "./components/columnselect.dry.hbs";
 
+// @ts-ignore
+import HomeIcon from "./assets/home.svg";
+// @ts-ignore
+import SettingsIcon from "./assets/settings.svg";
+// @ts-ignore
+import DebugIcon from "./assets/debug.svg";
+// @ts-ignore
+import PeopleIcon from "./assets/people.svg";
+// @ts-ignore
+import MeIcon from "./assets/me.svg";
+
 console.time('render');
 
 let header = Handlebars.compile(Header);
+let footer = Handlebars.compile(Footer);
 
 document.getElementById("app").innerHTML = HeaderContain + Main + FooterContain;
 
 document.getElementById("header-container").innerHTML = header({
   title: "Articleman",
   page: "Home",
+});
+
+document.getElementById("footer-container").innerHTML = footer({
+  isAdmin: true,
+  isDebugMode: true,
+  icons: {
+    home: HomeIcon,
+    settings: SettingsIcon,
+    debug: DebugIcon,
+    people: PeopleIcon,
+    me: MeIcon,
+  }
 });
 
 let bigTitle = Handlebars.compile(BigTitle);
