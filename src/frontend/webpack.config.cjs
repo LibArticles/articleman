bb80a5a7a2fa35e34a54ed7c11715fe6dffab10e
blue <dev@bluelinden.art>
2023-05-02 08:39:04 -0400

    work on webpacking ember output

--- src/frontend/webpack.config.cjs
+++ src/frontend/webpack.config.cjs
@@ -1,13 +1,32 @@
-// write a webpack config file with html-webpack-plugin
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
+const path = require("path");
+const glob = require("glob");
 
 module.exports = {
-  // The entry point for the bundle
-  entry: './src/frontend/index.js',
-  // The name of the output file
+  entry: glob.sync("./prebundle/assets/**.js").reduce(function (obj, el) {
+    obj[path.parse(el).name] = el;
+    return obj;
+  }, {}),
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./prebundle/index.html",
+      filename: "index.html",
+    }),
+    new HtmlInlineScriptPlugin({
+      scriptMatchPattern: "bundle.js",
+    }),
+  ],
 };
