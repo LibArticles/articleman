ff3b30d470014b8a0926cf2a3883712b50796e24
blue <dev@bluelinden.art>
2023-05-03 19:17:21 -0400

    start using external cdn

--- src/frontend/ember-cli-build.js
+++ src/frontend/ember-cli-build.js
@@ -13,7 +13,13 @@ module.exports = function (defaults) {
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
     packagerOptions: {
-      publicAssetURL: './',
+      publicAssetURL: EmberApp.env() === 'production' ? 'https://articleman-static.bluelinden.art/assets/' : '/',
+      webpackConfig: {
+        output: {
+          iife: false,
+        },
+      },
     },
   });
+  // return app.toTree();
 };
