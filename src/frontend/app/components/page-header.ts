d563a7adca111a7ad3493f833cb804edb8631925
blue <dev@bluelinden.art>
2023-05-08 18:55:39 -0400

    add page title component... will it work?

---
+++ src/frontend/app/components/page-header.ts
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import RouterService from '@ember/routing/router-service';
+import { action } from '@ember/object';
+
+export default class HeaderComponent extends Component {
+  @service router!: RouterService;
+  title: string = '';
+
+  constructor(owner: unknown, args: {}) {
+    super(owner, args);
+    this.setTitle();
+  }
+
+  @action
+  setTitle() {
+    const currentRouteName = this.router.currentRouteName;
+    switch (currentRouteName) {
+      case 'home':
+        this.title = 'Home Page';
+        break;
+      case 'about':
+        this.title = 'About Us';
+        break;
+      case 'contact':
+        this.title = 'Contact Us';
+        break;
+      default:
+        this.title = 'My Website';
+        break;
+    }
+    document.title = this.title;
+  }
+}
---
+++ src/frontend/app/routes/index.ts
@@ -0,0 +1,5 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+export default class IndexRoute extends Route {
+}
