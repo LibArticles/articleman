54cdc211f9754da06a3a72e1be4237b067c9991b
blue <dev@bluelinden.art>
2023-05-11 12:23:37 -0400

    update casement to 2.0.0

--- src/frontend/app/routes/index.ts
+++ src/frontend/app/routes/index.ts
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import CasementService from 'articleman-frontend/services/casement';
 
 export default class IndexRoute extends Route {
+  @service('casement') declare casement: CasementService;
+  init() {
+    super.init();
+    this.casement.inside.send('Hello, backend!');
+  }
 }
