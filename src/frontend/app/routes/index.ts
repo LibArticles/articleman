78d5cbce5ab75370425c7cdfc1721188d4fd687b
blue <dev@bluelinden.art>
2023-05-11 12:27:43 -0400

    fix compile error in syntax

--- src/frontend/app/routes/index.ts
+++ src/frontend/app/routes/index.ts
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 import CasementService from 'articleman-frontend/services/casement';
 
 export default class IndexRoute extends Route {
-  @service('casement') declare casement: CasementService;
+  @service('casement') casement!: CasementService;
   init() {
     super.init();
     this.casement.inside.send('Hello, backend!');
