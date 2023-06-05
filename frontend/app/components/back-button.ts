d12817b5ab9a8fdefba34600405d9978dbd0085d
blue <dev@bluelinden.art>
2023-06-05 11:55:19 -0400

    add back button, fix font requests, kill yarn

---
+++ frontend/app/components/back-button.ts
@@ -0,0 +1,24 @@
+// app/components/back-button.js
+
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import RouterHistoryService from 'frontend/services/route-history';
+import { action } from '@ember/object';
+
+export default class BackButtonComponent extends Component {
+  @service routerHistory!: RouterHistoryService;
+
+  // link to the previous route in the history stack
+
+  @computed('routerHistory.previousRoute')
+  get previousRoute() {
+    return this.routerHistory.previousRoute;
+  }
+
+  @action
+  back() {
+    this.routerHistory.back();
+  }
+
+}
