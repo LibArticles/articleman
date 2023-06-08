66c9944f98752d80a7562ec845fa0088a3a5dfbb
blue <dev@bluelinden.art>
2023-06-08 08:59:46 -0400

    switch to latest stable node for ci

--- .ci-deploy.sh
+++ .ci-deploy.sh
@@ -1,3 +1,4 @@
 export CLOUDFLARE_API_TOKEN=$CF_API_KEY >> /dev/null
 export CLOUDFLARE_ACCOUNT_ID=$CF_ACCT_ID >> /dev/null
-pnpm wrangler pages deploy frontend/dist --project-name=$CF_PAGES_NAME --branch=$CF_PAGES_BRANCH
+cd frontend
+pnpm wrangler pages deploy dist --project-name=$CF_PAGES_NAME --branch=$CF_PAGES_BRANCH
