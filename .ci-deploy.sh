e168a262586a1ba29514e109f720f8cf94f37522
blue <dev@bluelinden.art>
2023-06-08 08:48:10 -0400

    update ci building

--- .ci-deploy.sh
+++ .ci-deploy.sh
@@ -1,3 +1,3 @@
 export CLOUDFLARE_API_TOKEN=$CF_API_KEY >> /dev/null
 export CLOUDFLARE_ACCOUNT_ID=$CF_ACCT_ID >> /dev/null
-npx wrangler pages deploy frontend/dist --project-name=$CF_PAGES_NAME --branch=$CF_PAGES_BRANCH
+pnpm wrangler pages deploy frontend/dist --project-name=$CF_PAGES_NAME --branch=$CF_PAGES_BRANCH
