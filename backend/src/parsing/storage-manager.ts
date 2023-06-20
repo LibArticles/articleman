b27ab304759c8d1dc3ba828838a1775a37e544c2
blue <dev@bluelinden.art>
2023-06-20 18:41:08 -0400

    [skip ci] partially get rid of sializer, flesh out storage commit system

--- backend/src/parsing/storage-manager.ts
+++ backend/src/parsing/storage-manager.ts
@@ -1,4 +1,3 @@
-import Sializer from '@valentech/sializer';
 import fastChunkString from '@shelf/fast-chunk-string';
 
 type StorableValue = string | number | boolean | StorableObject | StorableArray | Blob | Date | null | undefined | Set<any> | Map<string, any>;
@@ -12,10 +11,33 @@ export default class StorageManager {
   }
 
   static async save(key: string, value: StorableValue) {
-    const serializedValue = Sializer.sia(value).toString();
 
+    // initialize variables
+    let serializedValue: string;
+    let dataType: string;
+
+    // serialize the value depending on its type
+    switch (typeof value) {
+      case "string":
+        serializedValue = value as string;
+        dataType = "string";
+      case "number":
+        serializedValue = (value as number).toString();
+        dataType = "number";
+      case "boolean":
+        serializedValue = (value as boolean).toString();
+        dataType = "boolean";
+      case "object" || "array":
+        serializedValue = JSON.stringify(value as StorableObject | StorableArray);
+        dataType = "object";
+    }
+
+    // initialize the airlock and storage
     const airlock: Record<string, string> = {};
     const storage = PropertiesService.getScriptProperties();
+
+    // compute the hash of the serialized value
+    const hash = Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, serializedValue, Utilities.Charset.UTF_8);
     
     // if the serialized value is too large, split it into multiple values
     if (serializedValue.length > 9200) {
@@ -24,7 +46,12 @@ export default class StorageManager {
         airlock[`ARTICLEMAN_STORAGE_${key}_${index}`] = chunk;
         index++;
       });
-      airlock[`ARTICLEMAN_STORAGE_${key}_LEGEND`] = index.toString();
+      airlock[`ARTICLEMAN_STORAGE_${key}_LEGEND`] = JSON.stringify({
+        length: index,
+        type: dataType,
+        note: "DO NOT MODIFY THIS VALUE UNLESS YOU REEEEALLLY KNOW WHAT YOU'RE DOING! CHANGING THIS VALUE COULD COMPLETELY BREAK ARTICLEMAN AND REQUIRE A RESET OF ALL DATA!",
+        digest: hash,
+      });
     } else {
       airlock[`ARTICLEMAN_STORAGE_${key}`] = serializedValue;
     }
