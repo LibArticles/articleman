5d29e0a1eebaf3f3d8635697cde65d67e6dbb49b
blue <dev@bluelinden.art>
2023-07-05 00:01:04 -0400

    solidify AMDS classes and add credit for portions of a stylesheet

--- backend/src/parsing/datasets/classes/container.ts
+++ backend/src/parsing/datasets/classes/container.ts
@@ -3,6 +3,15 @@ import { v4 as uuidv4 } from "uuid";
 import AMGroup from "./group";
 import AMObject from "./object";
 
+/*
+  # ArticleManContainer
+  The AMContainer class is the container for every bit of data contained within Articleman's DataSet managemenet system.
+
+  Links are stored in the AMGroup class, as that makes them nice and global and avoids the need to store them in the AMObject class. The object knows the ID of the link, and that's it.
+
+  Caches are just cached lookups 
+*/
+
 export default class AMContainer {
   spreadsheetID: string;
 
@@ -21,8 +30,12 @@ export default class AMContainer {
     }
   }
 
+  // link two or more objects to each other
   async linkObjects(...objects: Array<AMObject>) {
     const linkID = uuidv4();
+    if (objects.length < 2) {
+      return;
+    }
     if (!this.links) {
       this.links[linkID] = [];
     }
@@ -37,15 +50,24 @@ export default class AMContainer {
     }
     return linkID;
   }
-  
 
+  // turn all stored data into a JSON object
+  crystallize() {
+    const groups: Record<string, any> = {};
+    for (const groupID of Object.keys(this.groups)) {
+      groups[groupID] = this.groups[groupID].crystallize();
+    }
+    return groups;
+  }
+  
+  // take a container constructor and return a new container
   constructor(input: containerConstructor) {
     this.caches = input.caches;
     this.spreadsheetID = input.spreadsheetID;
     for (const groupID of Object.keys(input.data)) {
       const groupData = input.data[groupID];
 
-      this.groups[groupID] = new AMGroup(this, groupID, input.mappings[groupID], groupData);
+      this.groups[groupID] = new AMGroup({ container: this, id: groupID, mappings: input.mappings[groupID], objects: groupData });
     }
   }
   
@@ -54,6 +76,7 @@ export default class AMContainer {
 interface containerConstructor {
   caches: Record<string, Record<string, {group: string, index: number}>>,
   mappings: Record<string, Record<string, string>>, 
+  links: Record<string, Array<string>>,
   spreadsheetID: string, 
   data: Record<string, Record<string, any>[]>
 }
