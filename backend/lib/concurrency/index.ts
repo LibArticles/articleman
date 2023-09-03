ea106f68e68fdb1363af491bbe2db5a822d34ea8
blue <dev@bluelinden.art>
2023-09-02 20:07:23 -0400

    add TSDoc to lib/concurrency for easier usage in IDEs

--- backend/lib/concurrency/index.ts
+++ backend/lib/concurrency/index.ts
@@ -13,6 +13,12 @@ export class Turnstile {
 	type: string;
 	id: string;
 
+	/**
+	 * Attempts to enter a turnstile.
+	 * @param timeOut How long should the executor wait in line for the others to exit?
+	 * @param error Should it throw an error if it times out?
+	 * @returns A promise with true or false, for whether the executor could enter the turnstile
+	 */
 	async enter(timeOut: number, error?: boolean) {
 		if (!this.get(this.name)) {
 			this.set(this.name, [this.id]);
@@ -36,10 +42,16 @@ export class Turnstile {
 		}
 	}
 
+	/**
+	 * Exits a turnstile, leaving it for the next in line or closing it.
+	 */
 	exit() {
 		const arrayToDeleteMyNameIn: Array<string> = this.get(this.name);
 		const arrayWithoutMyName = arrayToDeleteMyNameIn.splice(arrayToDeleteMyNameIn.indexOf(this.id));
+		if (arrayWithoutMyName.length !== 0)
 		this.set(this.name, arrayWithoutMyName);
+		else
+		this.delete(this.name)
 	}
 
 	private get(key: string) {
@@ -69,6 +81,7 @@ export class Turnstile {
 
 
 
+
 	private delete(key: string) {
 		switch (this.type) {
 			case 'document':
@@ -83,6 +96,11 @@ export class Turnstile {
 		}
 	}
 
+	/**
+	 * Makes a turnstile to manage concurrency
+	 * @param name The name of the turnstile to create
+	 * @param type The scope of the turnstile
+	 */
 	constructor(name: string, type: 'document' | 'script' | 'user') {
 		this.name = name;
 		this.type = type;
