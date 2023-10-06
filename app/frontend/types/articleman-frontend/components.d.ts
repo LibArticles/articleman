b97e45655dc393b8e49b8b795dfd69ff4c2e1bcc
blue <dev@bluelinden.art>
2023-10-06 12:38:34 -0400

    add notificationcenter

--- app/frontend/types/articleman-frontend/components.d.ts
+++ app/frontend/types/articleman-frontend/components.d.ts
@@ -1,26 +1,24 @@
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
 
-// TODO: UPDATE EMBER-INTL TO WHATEVER VERSION FIXES GLINT SUPPORT
-// import type EmberIntlRegistry from 'ember-intl/template-registry';
 
-import BackButtonComponent from "frontend/components/back-button";
-import MainMenuComponent from "frontend/components/main-menu";
-import PageHeaderComponent from "frontend/components/page-header";
-import NavButtonComponent from "frontend/components/nav-button";
-import svgJar from "ember-svg-jar/helpers/svg-jar";
+import BackButtonComponent from 'frontend/components/back-button';
+import MainMenuComponent from 'frontend/components/main-menu';
+import PageHeaderComponent from 'frontend/components/page-header';
+import NavButtonComponent from 'frontend/components/nav-button';
+import type EmberIntlRegistry from 'ember-intl/template-registry';
+import EmberSvgJarRegistry from 'ember-svg-jar/template-registry';
+import NotificationCenterComponent from 'frontend/components/notification-center';
+import NotificationComponent from 'frontend/components/notification';
 
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry /* extends EmberIntlRegistry */  /* other addon registries */ {
-
-
-    BackButton: typeof BackButtonComponent;
-    MainMenu: typeof MainMenuComponent;
-    PageHeader: typeof PageHeaderComponent;
-    NavButton: typeof NavButtonComponent;
-
-    "svg-jar": typeof svgJar;
-
-    t: (key: string, options: Record<string, unknown>) => string;
-  }
+	export default interface Registry
+		extends EmberIntlRegistry, EmberSvgJarRegistry {
+		BackButton: typeof BackButtonComponent;
+		MainMenu: typeof MainMenuComponent;
+		PageHeader: typeof PageHeaderComponent;
+		NavButton: typeof NavButtonComponent;
+		NotificationCenter: typeof NotificationCenterComponent
+		Notification: typeof NotificationComponent
+	}
 }
