71e6b0d838d00fade5a6c18e8a3b37802d15d8a1
blue <dev@bluelinden.art>
2023-10-06 12:37:06 -0400

    delete legacy comms infra

--- app/backend/src/comms/command-parse.ts
+++
@@ -1,6 +0,0 @@
-import recognizedCommands from '../../../shared/recognized-commands';
-import DataManager from './data-operator';
-
-export default class CommandParser {
-	parse(command: string, data: any) {}
-}
