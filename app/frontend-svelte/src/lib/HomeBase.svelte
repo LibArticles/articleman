64d60443f760bd1d4c382eb92563db38eb81b7e9
blue <dev@bluelinden.art>
2023-10-08 23:28:31 -0400

    begin migration to svelte

---
+++ app/frontend-svelte/src/lib/HomeBase.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	function toggleExpand() {
+		
+	}
+
+	let state: 'toast' | 'open' | 'closed' | 'hidden' = 'closed';
+</script>
+
+<div class="home-base {state}">
+	<button class="home-base-toggler" on:click={toggleExpand} >
+		<div class="icon">
+			
+		</div>
+	</button>
+</div>
