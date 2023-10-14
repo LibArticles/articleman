3a3625f5890944e4a1618d66dc067369af239867
blue <dev@bluelinden.art>
2023-10-13 20:55:26 -0400

    start inner casement support

---
+++ app/frontend/src/lib/comms/casement.ts
@@ -0,0 +1,35 @@
+import { Inside } from "casement";
+
+
+
+export class CasementManager {
+	private casement: Inside;
+
+	send(data: any, actionName?: string) {
+		this.casement.send(data, actionName);
+	}
+
+	request(data: any, actionName?: string) {
+		return this.casement.request(data, actionName);
+	}
+
+
+	
+
+
+	constructor() {
+		this.casement = new Inside({
+			allowedDomain: '*',
+			name: 'socketeer',
+			onReady: () => {
+				
+			}
+		});
+
+
+	}
+}
+
+const casementManager = new CasementManager();
+export default casementManager;
+
